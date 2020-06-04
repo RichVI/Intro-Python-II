505e24279a050814a7d378e9181def02eee45bc2
RichVI <richardwang2050@gmail.com>
2020-06-03 18:29:07 -0700

    MVP

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -22,6 +23,7 @@ earlier adventurers. The only exit is to the south."""),
 }
 
 
+
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
@@ -37,15 +39,37 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+def move_player(player_input):
+    next_room = getattr(richard.current_room, f"{player_input}_to")
+    if next_room == "invalid":
+        print("==Area is out of bounds==")
+    else: 
+        richard.current_room = next_room
+    game_loop()
 
+
+# Make a new player object that is currently in the 'outside' room.
+richard = Player("Richard", room["outside"])
+print(f"\nPlayer Name: {richard.name}, welcome to the game! \n")
 # Write a loop that:
 #
+def game_loop():
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+    print(f"Location: {richard.current_room.name}\n{richard.current_room.description}\n\nWhere do you go next? [n] [e] [s] [w] [q] for quit]")
+    player_input = input()
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
+    if player_input == "q":
+        print("==Game Over==")
+    elif player_input == "n" or player_input == "e" or player_input == "s" or player_input == "w":
+        move_player(player_input)
+    else:
+        print("Please enter a valid direction, or q to quit the game!")
+
 # If the user enters "q", quit the game.
+
+game_loop()
